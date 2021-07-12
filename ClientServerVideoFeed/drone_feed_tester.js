ef4b4dd27b92542c59515c2569cd386532bd92ca
Gaurav Singh <gp5in9h@gmail.com>
2021-07-13 03:00:35 +0530

    file structure changed pt 2

--- ClientServerVideoFeed/drone_feed_tester.js
+++
@@ -1,7 +0,0 @@
-const child_process = require('child_process')
-
-let py = child_process.spawn('python3', ['drone_feed.py', '-p 8080']);
-
-py.stdout.on('data', (data) => {
-    console.log(data.toString())
-})
